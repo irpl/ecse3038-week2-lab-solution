c339ae5a50a659baab89381186db926977ea782a
Phillip Logan <philo3194@gmail.com>
2024-02-05 17:50:24 -0500

    added a fucntion called temperature_check

--- app.py
+++ app.py
@@ -11,3 +11,14 @@ def potential_divider(v, resistors):
   for r in resistors:
     vr = (v * r)/sum(resistors)
     print(str(vr) + "v")
+
+def temperature_check(temp, unit):
+  if (unit == "F") or (unit == "f"):
+    temp = (temp - 32) * 5/9 # this temp will be in c
+
+  if temp > 40:
+    print("hyper!!!!")
+  elif temp < 36:
+    print("hypo!!!!")
+  else:
+    print("normal, a lie?")
